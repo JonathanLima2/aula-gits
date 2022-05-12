90434fbd88af40500d60d321038ade8c5f87f2be
Jonathan Lima <jdsl@icomp.ufam.edu.br>
2022-05-12 19:46:56 -0400

    alterei a main

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[]args){
-        System.out.println("hello world");
+        System.out.println("hello worl");
        /* Scanner s = new Scanner(System.in);
         pessoa p = new pessoa("Alvin", "esquilo@gmail.com", 15);
         p.print();
