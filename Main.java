7078a360db1a6eaf0ac3221efe361bebb43a1081
Jonathan Lima <jdsl@icomp.ufam.edu.br>
2022-05-12 20:20:26 -0400

    alterações na classe main

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[]args){
-        System.out.println("hello worl");
-       /* Scanner s = new Scanner(System.in);
-        pessoa p = new pessoa("Alvin", "esquilo@gmail.com", 15);
-        p.print();
-        s.close();*/
+        Scanner s = new Scanner(System.in);
+        jsl pp = new jsl();
+//        pessoa p = new pessoa("Alvin", "esquilo@gmail.com", 15);
+        pp.print();
+        s.close();
     }
     
 }
